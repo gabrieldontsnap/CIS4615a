1ec5522a8940a7bdbc87a41909e15136017c02d3
gabrieldontsnap <67400913+gabrieldontsnap@users.noreply.github.com>
2020-10-25 21:42:36 -0400

    Update R08_MSC01_J.java

--- R08_MSC01_J.java
+++ R08_MSC01_J.java
@@ -1,3 +1,8 @@
-public int nop() {
- while (true) {}
+public final int DURATION=10000; // in milliseconds
+
+public void nop() throws InterruptedException {
+ while (true) {
+ // Useful operations
+ Thread.sleep(DURATION);
+ }
 }
